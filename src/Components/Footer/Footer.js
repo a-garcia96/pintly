2aafcb6bc11bd42a417c636a2dbbdcc16f9baee7
Alex Garcia <alex.g9415@gmail.com>
2020-11-24 14:36:31 -0800

    Add Navigation Links

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -26,14 +26,14 @@ const footer = props => (
                 <Col sm="auto" lg={4}>
                     <h5>Social</h5>
                     <ul className={styles.socialFooter}>
-                        <li><FontAwesomeIcon size="lg" icon={faInstagram} /></li>
-                        <li><FontAwesomeIcon size="lg" icon={faLinkedin} /></li>
-                        <li><FontAwesomeIcon size="lg" icon={faGithub} /></li>
+                        <li><a href="https://www.instagram.com/alex_gaarciaa/"><FontAwesomeIcon size="lg" icon={faInstagram} /></a></li>
+                        <li><a href="https://www.linkedin.com/in/agarcia96"><FontAwesomeIcon size="lg" icon={faLinkedin} /></a></li>
+                        <li><a href="https://github.com/a-garcia96"><FontAwesomeIcon size="lg" icon={faGithub} /></a></li>
                     </ul>
                 </Col>
                 <Col sm="auto" lg={4}>
                     <h5>API Usage</h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed orci eget urna imperdiet euismod. Quisque lobortis id nisi eget facilisis. Ut iaculis quam vitae magna aliquet rhoncus. Aenean nec convallis ipsum. Aliquam erat volutpat. Suspendisse in interdum leo.</p>
+                    <p>All data is provide by BreweryDB. To learn more about the API please refer to the <a href="https://brewerydb.com/developers/docs">site documentation</a></p>
                 </Col>
             </Row>
         </Container>
