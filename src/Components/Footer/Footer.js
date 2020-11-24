af7d93471745682de87d81f2087cf6b98f5f2da5
Alex Garcia <alex.g9415@gmail.com>
2020-11-23 21:36:01 -0800

    Copyright Info

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import  Row  from 'react-bootstrap/Row';
+import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
-import styles from  './Footer.module.css'
+import styles from './Footer.module.css'
 
 const date = new Date();
 
@@ -13,7 +13,7 @@ const footer = props => (
     <footer>
         <Container>
             <Row>
-                <Col>
+                <Col sm="auto" lg={4}>
                     <h5>Navigation</h5>
                     <ul>
                         <li>Home</li>
@@ -21,7 +21,7 @@ const footer = props => (
                         <li>Breweries</li>
                     </ul>
                 </Col>
-                <Col>
+                <Col sm="auto" lg={4}>
                     <h5>Social</h5>
                     <ul className={styles.socialFooter}>
                         <li><FontAwesomeIcon size="lg" icon={faInstagram} /></li>
@@ -29,15 +29,17 @@ const footer = props => (
                         <li><FontAwesomeIcon size="lg" icon={faGithub} /></li>
                     </ul>
                 </Col>
-                <Col>
+                <Col sm="auto" lg={4}>
                     <h5>API Usage</h5>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed orci eget urna imperdiet euismod. Quisque lobortis id nisi eget facilisis. Ut iaculis quam vitae magna aliquet rhoncus. Aenean nec convallis ipsum. Aliquam erat volutpat. Suspendisse in interdum leo.</p>
                 </Col>
             </Row>
-            <Row>
-                <h4>@{date.getFullYear()}</h4>
-            </Row>
         </Container>
+        <section className={`${styles.footerCopyright}`}>
+            <Container>
+                <p className="text-center">Copyright {date.getFullYear()} @ Alex Garcia</p>
+            </Container>
+        </section>
     </footer>
 );
 
