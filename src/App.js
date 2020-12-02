e761e0b0d96389092c41635d9c01560ff0418e7e
Alex Garcia <alex.g9415@gmail.com>
2020-12-01 22:07:56 -0800

    React-Meta-Tags
    Added NPM package "React-Meta-Tags" to be able to add meta tags to site.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import DocumentMeta from 'react-document-meta';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import ScrollToTop from './Components/scrollToTop/scrollToTop';
 
@@ -9,13 +10,28 @@ import Breweries from './Pages/Breweries/Breweries';
 
 
 function App() {
+  const meta = {
+    title: 'Pintly App',
+    description: 'Learn about Beers and Breweries',
+    meta: {
+      charset: 'utf-8',
+      name: {
+        keywords: 'react, beers, pint, app, brewery, brewerydb',
+        viewport: 'width=device-width, initial-scale=1.0'
+      }
+    }
+
+  }
+
   return (
-    <Router>
-      <ScrollToTop />
-      <Route path="/" exact component={Home}/>
-      <Route path="/beers" component={Beers}/>
-      <Route path="/breweries" component={Breweries} />
-    </Router>
+    <DocumentMeta {...meta}>
+      <Router>
+        <ScrollToTop />
+        <Route path="/" exact component={Home} />
+        <Route path="/beers" component={Beers} />
+        <Route path="/breweries" component={Breweries} />
+      </Router>
+    </DocumentMeta>
   );
 }
 
