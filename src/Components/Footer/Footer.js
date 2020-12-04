ac37064b60139d6d0f916c517682bd01916809e3
Alex Garcia <alex.g9415@gmail.com>
2020-12-03 18:33:21 -0800

    footer styling

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -15,24 +15,24 @@ const footer = props => (
     <footer>
         <Container>
             <Row>
-                <Col sm="auto" lg={4}>
-                    <h5>Navigation</h5>
+                <Col className={styles.footerNavigation} sm={12} lg={4}>
+                    <h3>Navigation</h3>
                     <ul>
                         <li><Link to="/">Home</Link></li>
                         <li><Link to="/beers">Beers</Link></li>
                         <li><Link to="/breweries">Breweries</Link></li>
                     </ul>
                 </Col>
-                <Col sm="auto" lg={4}>
-                    <h5>Social</h5>
+                <Col className={styles.footerSocialLinks} sm={12} lg={4}>
+                    <h3>Follow Me</h3>
                     <ul className={styles.socialFooter}>
-                        <li><a href="https://www.instagram.com/alex_gaarciaa/"><FontAwesomeIcon size="lg" icon={faInstagram} /></a></li>
-                        <li><a href="https://www.linkedin.com/in/agarcia96"><FontAwesomeIcon size="lg" icon={faLinkedin} /></a></li>
-                        <li><a href="https://github.com/a-garcia96"><FontAwesomeIcon size="lg" icon={faGithub} /></a></li>
+                        <li><a href="https://www.instagram.com/alex_gaarciaa/"><FontAwesomeIcon size="2x" icon={faInstagram} /></a></li>
+                        <li><a href="https://www.linkedin.com/in/agarcia96"><FontAwesomeIcon size="2x" icon={faLinkedin} /></a></li>
+                        <li><a href="https://github.com/a-garcia96"><FontAwesomeIcon size="2x" icon={faGithub} /></a></li>
                     </ul>
                 </Col>
-                <Col sm="auto" lg={4}>
-                    <h5>API Usage</h5>
+                <Col className={styles.footerApiDetails} sm="auto" lg={4}>
+                    <h3>API Usage</h3>
                     <p>All data is provide by BreweryDB. To learn more about the API please refer to the <a href="https://brewerydb.com/developers/docs">site documentation</a></p>
                 </Col>
             </Row>
