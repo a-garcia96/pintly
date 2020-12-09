96370c714bd79bd44beb5b7a0c95213b90615764
Alex Garcia <alex.g9415@gmail.com>
2020-12-08 16:11:38 -0800

    Mobile First Styling

--- src/Components/Cards/Card/Card.js
+++ src/Components/Cards/Card/Card.js
@@ -9,11 +9,11 @@ const card = (props) => (
     <Card className={Styles.card} data-id={props.searchKey}>
         <Card.Img variant="top" src={props.img} />
         <Card.Body>
-            <Card.Title>{props.title}</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">{props.subtitle}</Card.Subtitle>
-            <Card.Text><strong>{props.info}%</strong></Card.Text>
+            <Card.Title className={Styles.cardMyTitle}>{props.title}</Card.Title>
+            <Card.Subtitle className={`mb-2 text-muted ${Styles.cardMySubtitle}`}>{props.subtitle}</Card.Subtitle>
+            <Card.Text className={Styles.cardMyText}><strong>{props.info}%</strong></Card.Text>
         </Card.Body>
-        <Button onClick={props.modalHandler} variant="secondary">Learn More</Button>
+        <Button className={Styles.cardMyButton} onClick={props.modalHandler} variant="secondary">Learn More</Button>
     </Card>
 );
 
