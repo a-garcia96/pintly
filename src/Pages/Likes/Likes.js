42a928c97c1edacb727c0bff3faf99f05b9f3508
Alex Garcia <alex.g9415@gmail.com>
2020-12-10 14:58:05 -0800

    convert to functional component

--- src/Pages/Likes/Likes.js
+++ src/Pages/Likes/Likes.js
@@ -1,17 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+import Context from '.././../Components/Context/Context';
 
 import Layout from '../../Components/Layout/Layout';
 
-class Likes extends Component {
-
-
-    render() {
-        return (
-            <Layout>
+const likes = props => (
+    <Context.Consumer>
+        <Layout>
+            <Container>
                 <h1>Your saved beers and breweries!</h1>
-            </Layout>
-        );
-    }
-}
+            </Container>
+        </Layout>
+    </Context.Consumer>
+);
 
-export default Likes;
+export default likes;
